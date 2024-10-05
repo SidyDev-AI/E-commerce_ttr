e716c3b23a921d9c9f8db0a2e8dbb6df70156d80
SidyDev-AI <sidydev.io@gmail.com>
2024-10-04 23:40:25 -0300

    Finalizando por hj

---
+++ essencial/frontend/src/core/produto/Produto.ts
@@ -0,0 +1,14 @@
+import Especificacoes from "./Especificacoes"
+
+export default interface Produto {
+  id: number
+  nome: string
+  descricao: string
+  marca: string
+  modelo: string
+  imagem: string
+  videoReview: string
+  nota: number
+  tags: string[]
+  especificacoes: Especificacoes
+}
